0fdb270eb640a7ea679a868e93494053ed2e1be3
Evandro Ribeiro <ribeiroevandro@live.com>
2023-04-16 02:11:24 -0300

    chore: create layout template and refactor in api

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -1,40 +1,14 @@
-
-import express from 'express';
+import express, { Response } from "express";
 
 var app = express();
 
-const users = [
-  {
-      id: 1,
-      name: "Lucas"
-  },
-  {
-      id: 2,
-      name: "Eric"
-  },
-  {
-      id: 3,
-      name: "Ana"
-  },
-];
-
-
-app.get('/', function(req:any, res:any){
-  res.send('Hello!');
-});
-
-app.get('/users', function(req:any, res:any){
-  res.send(users);
-});
-
-app.get('/users/:userId', function(req:any, res:any){
-  const user = users.filter((user) => user.id == req.params.userId);
-  res.send(user);
+app.get("/", function (res: Response) {
+  res.send("Bohr Express template");
 });
 
 if (!module.parent) {
   app.listen(3000);
-  console.log('Express started on port 3000');
+  console.log("Express started on port 3000");
 }
 
 export default app;
