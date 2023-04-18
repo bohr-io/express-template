60febce6d7f5d0f637296c1592ea81734df3b979
Evandro Ribeiro <ribeiroevandro@live.com>
2023-04-18 15:48:48 -0300

    feat: re-include examples and improments in footer

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -2,8 +2,24 @@ import express, { Response, Request } from "express";
 
 var app = express();
 
+const users = [
+  { id: 1, name: "Lucas" },
+  { id: 2, name: "Eric" },
+  { id: 3, name: "Ana" },
+];
+
+
 app.get("/", function (req: Request, res: Response) {
   res.send("Bohr Express template");
+});
+
+app.get("/users", function (req: Request, res: Response) {
+  res.send(users);
+});
+
+app.get("/users/:userId", function (req: Request, res: Response) {
+  const user = users.find((user) => user.id === parseInt(req.params.userId));
+  res.send(user);
 });
 
 if (!module.parent) {
